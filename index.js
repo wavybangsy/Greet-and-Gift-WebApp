6ac912348d67fa1025b4f8cd2b7cdb48434f701e
wavybangsy <mary_erika_culala@dlsu.edu.ph>
2023-08-15 22:27:21 +0800

    starter code

--- index.js
+++ index.js
@@ -2,26 +2,6 @@
 const express = require("express");
 const app = express(); // express app instance
 
-app.get("/", (req, res) => {
-  res.statusCode = 200;
-  res.send(`
-        <!DOCTYPE HTML>
-        <html>
-            <head>
-            <title>Sample WebPage</title>
-            </head>
-
-            <body>
-                <h1>Hi! This is the URL: ${req.url}</h1>
-            </body>
-        </html>
-    `);
-});
-
-app.get("/home", (req, res) => {
-  res.redirect("/");
-});
-
 // activate the app instance
 app.listen(3000, () => {
   console.log("Express app is now listening on Port 3000...");
